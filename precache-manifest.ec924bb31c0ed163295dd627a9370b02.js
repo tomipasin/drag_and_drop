fc951cc923793589026eb8548d30cd86dc1c2c7d
tomipasin <tomipasin@gmail.com>
2021-04-28 12:24:04 -0300

    Updates

---
+++ precache-manifest.ec924bb31c0ed163295dd627a9370b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08c0f782456b5c33fd5e13e17159ea7",
+    "url": "/drag_and_drop/index.html"
+  },
+  {
+    "revision": "6a74b41a4daac996dc38",
+    "url": "/drag_and_drop/static/css/main.922aa3c5.chunk.css"
+  },
+  {
+    "revision": "18406dd014b64511c35b",
+    "url": "/drag_and_drop/static/js/2.7045e303.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/drag_and_drop/static/js/2.7045e303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a74b41a4daac996dc38",
+    "url": "/drag_and_drop/static/js/main.21d76b81.chunk.js"
+  },
+  {
+    "revision": "a58acfaf2a1733803ad2",
+    "url": "/drag_and_drop/static/js/runtime-main.ad12770f.js"
+  }
+]);
