63d341e80eb62067f4d96fb907b9124935b624ec
tomipasin <tomipasin@gmail.com>
2021-04-28 14:05:05 -0300

    Updates

--- precache-manifest.079a45586395670003df59168b2efbc4.js
+++ precache-manifest.079a45586395670003df59168b2efbc4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61d4a08fb6ae8092b8c3999a043ccf07",
+    "revision": "2f14cb48e74233638c3e66c2256393e6",
     "url": "/drag_and_drop/index.html"
   },
   {
-    "revision": "5bcef33e8e0dde1fc0cf",
+    "revision": "fa85fd656778331ce185",
     "url": "/drag_and_drop/static/css/main.922aa3c5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/drag_and_drop/static/js/2.7045e303.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5bcef33e8e0dde1fc0cf",
-    "url": "/drag_and_drop/static/js/main.065eb15d.chunk.js"
+    "revision": "fa85fd656778331ce185",
+    "url": "/drag_and_drop/static/js/main.098bcca4.chunk.js"
   },
   {
     "revision": "a58acfaf2a1733803ad2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drag_and_drop/precache-manifest.55be90aea304f9f775169fb5514591fa.js"
+  "/drag_and_drop/precache-manifest.079a45586395670003df59168b2efbc4.js"
 );
 
 self.addEventListener('message', (event) => {
